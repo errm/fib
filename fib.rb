d5832705001fdef0c1d69a68e5bd91309ffb763a
Ed Robinson <edward-robinson@cookpad.com>
2018-01-21 17:17:05 +0000

    Make path simpler

--- fib.rb
+++ fib.rb
@@ -1,7 +1,11 @@
-require 'sinatra'
+require "sinatra"
 
 f = ->(x){ x < 2 ? x : f[x-1] + f[x-2] }
 
-get '/fib/:num' do
-  f[params['num'].to_i].to_s
+get "/:num" do
+  f[params.fetch("num").to_i].to_s
+end
+
+get "/" do
+  f[1].to_s
 end
